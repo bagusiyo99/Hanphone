68d5d0b01b44725410a58b722fee52973c4f5a8d
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-02-01 21:05:46 +0700

    perbaikan

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -34,7 +34,7 @@
                             <li class="nav-item">
                                 <a href="/admin/about"
                                     class="nav-link  {{ Request::is('admin/about*') ? 'active' : '' }}">
-                                    <i class="nav-icon fas fa-calendar"></i>
+                                    <i class="nav-icon fas fa-clone"></i>
                                     <p>
                                         About
                                         {{-- <span class="right badge badge-danger">New</span> --}}
@@ -52,7 +52,7 @@
                             <li class="nav-item">
                                 <a href="/admin/informasi"
                                     class="nav-link  {{ Request::is('admin/informasi*') ? 'active' : '' }}">
-                                    <i class="nav-icon fas fa-calendar"></i>
+                                    <i class="nav-icon fa fa-file-contract"></i>
                                     <p>
                                         Informasi
                                         {{-- <span class="right badge badge-danger">New</span> --}}
@@ -70,7 +70,7 @@
                             <li class="nav-item">
                                 <a href="/admin/banner"
                                     class="nav-link  {{ Request::is('admin/banner*') ? 'active' : '' }}">
-                                    <i class="nav-icon fas fa-calendar"></i>
+                                    <i class="nav-icon fas fa-mobile"></i>
                                     <p>
                                         Banner
                                         {{-- <span class="right badge badge-danger">New</span> --}}
@@ -88,7 +88,7 @@
                             <li class="nav-item">
                                 <a href="/admin/promo"
                                     class="nav-link  {{ Request::is('admin/promo*') ? 'active' : '' }}">
-                                    <i class="nav-icon fas fa-calendar"></i>
+                                    <i class="nav-icon fa fa-bullhorn"></i>
                                     <p>
                                         Promo
                                         {{-- <span class="right badge badge-danger">New</span> --}}
@@ -106,7 +106,7 @@
                with font-awesome or any other icon font library -->
                             <li class="nav-item">
                                 <a href="#" class="nav-link  {{ Request::is('admin/blog*') ? 'active' : '' }}">
-                                    <i class="nav-icon fas fa-file"></i>
+                                    <i class="nav-icon fas fa-briefcase"></i>
                                     <p>
                                         Handphone
                                         <i class="right fas fa-angle-left"></i>
@@ -115,21 +115,21 @@
                                 <ul class="nav nav-treeview">
                                     <li class="nav-item">
                                         <a href="/admin/samsung" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Samsung</p>
                                         </a>
                                     </li>
 
                                     <li class="nav-item">
                                         <a href="/admin/asus" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Asus </p>
                                         </a>
                                     </li>
 
                                     <li class="nav-item">
                                         <a href="/admin/xiomi" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Xiomi </p>
                                         </a>
                                     </li>
@@ -137,35 +137,35 @@
 
                                     <li class="nav-item">
                                         <a href="/admin/vivo" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Vivo </p>
                                         </a>
                                     </li>
 
                                     <li class="nav-item">
                                         <a href="/admin/oppo" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Oppo </p>
                                         </a>
                                     </li>
 
                                     <li class="nav-item">
                                         <a href="/admin/infinix" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Infinix </p>
                                         </a>
                                     </li>
 
                                     <li class="nav-item">
                                         <a href="/admin/iphone" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Iphone </p>
                                         </a>
                                     </li>
 
                                     <li class="nav-item">
                                         <a href="/admin/hplain" class="nav-link">
-                                            <i class="far fa-circle nav-icon"></i>
+                                            <i class="fa fa-duotune fa-pen nav-icon"></i>
                                             <p>Lainnya </p>
                                         </a>
                                     </li>
@@ -176,7 +176,7 @@
 
                     <li class="nav-item">
                         <a href="#" class="nav-link  {{ Request::is('admin/blog*') ? 'active' : '' }}">
-                            <i class="nav-icon fas fa-file"></i>
+                            <i class="nav-icon fa fa-box"></i>
                             <p>
                                 Accessories
                                 <i class="right fas fa-angle-left"></i>
@@ -185,21 +185,21 @@
                         <ul class="nav nav-treeview">
                             <li class="nav-item">
                                 <a href="/admin/charger" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
+                                    <i class="fa  fa-forward nav-icon"></i>
                                     <p>Charger</p>
                                 </a>
                             </li>
 
                             <li class="nav-item">
                                 <a href="/admin/casing" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
+                                    <i class="fa  fa-forward nav-icon"></i>
                                     <p>Casing </p>
                                 </a>
                             </li>
 
                             <li class="nav-item">
                                 <a href="/admin/pb" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
+                                    <i class="fa  fa-forward nav-icon"></i>
                                     <p>Power Bank </p>
                                 </a>
                             </li>
@@ -207,14 +207,14 @@
 
                             <li class="nav-item">
                                 <a href="/admin/usb" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
+                                    <i class="fa  fa-forward nav-icon"></i>
                                     <p>Usb </p>
                                 </a>
                             </li>
 
                             <li class="nav-item">
                                 <a href="/admin/lainaksesoris" class="nav-link">
-                                    <i class="far fa-circle nav-icon"></i>
+                                    <i class="fa  fa-forward nav-icon"></i>
                                     <p>Lainnya </p>
                                 </a>
                             </li>
